4a28e692bd418ff265a08153b004ff9207cb7f68
Angelo-CPE <angelo.dscpl@gmail.com>
2024-05-19 00:56:53 +0800

    Add files via upload

--- cswp.py
+++ cswp.py
@@ -9,37 +9,50 @@ Original file is located at
 
 import streamlit as st
 import tensorflow as tf
-
-@st.cache(allow_output_mutation=True)
-def load_model():
-  model=tf.keras.models.load_model('CS_WP_Deployment2.h5')
-  return model
-model=load_model()
-st.write("""
-# CS - Weather Prediction System"""
-)
-file=st.file_uploader("Choose a weather image from computer",type=["jpg","png"])
-
-import cv2
-from PIL import Image,ImageOps
 import numpy as np
+from PIL import Image, ImageOps
 
-def import_and_predict(image_data,model):
-    size=(128,128)
-    image=ImageOps.fit(image_data,size,Image.ANTIALIAS)
-    img=np.asarray(image)
-    img=cv2.resize(img, (128,128), interpolation=cv2.INTER_NEAREST)
-    if img.ndim==3 and img.shape[2]==3:
-      img=cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    img_reshape=img.reshape((1,)+img.shape+(1,))
-    prediction=model.predict(img_reshape)
+# Caching the model loading
+@st.cache_resource
+def load_model():
+    model = tf.keras.models.load_model('CS_WP_Deployment2.h5')
+    return model
+
+model = load_model()
+
+st.write("""
+# CS - Weather Prediction System
+""")
+
+file = st.file_uploader("Choose a weather image from computer", type=["jpg", "png"])
+
+def import_and_predict(image_data, model):
+    size = (128, 128)
+    # Resize image
+    image = ImageOps.fit(image_data, size, Image.LANCZOS)
+    img = np.asarray(image)
+
+    # Convert to grayscale if necessary
+    if img.ndim == 3 and img.shape[2] == 3:
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+
+    # Normalize the image
+    img = img / 255.0
+
+    # Reshape the image for the model
+    img_reshape = img.reshape((1, 128, 128, 1))
+
+    # Predict
+    prediction = model.predict(img_reshape)
+
     return prediction
+
 if file is None:
     st.text("Please upload an image file")
 else:
-    image=Image.open(file)
-    st.image(image,use_column_width=True)
-    prediction=import_and_predict(image,model)
-    class_names=['Shine', 'Rain', 'Sunrise', 'Cloudy']
-    string="OUTPUT : "+class_names[np.argmax(prediction)]
+    image = Image.open(file)
+    st.image(image, use_column_width=True)
+    prediction = import_and_predict(image, model)
+    class_names = ['Shine', 'Rain', 'Sunrise', 'Cloudy']
+    string = "OUTPUT : " + class_names[np.argmax(prediction)]
     st.success(string)
